e7e16fae9fbe2b014d30f600315b77fcbfa6a32a
Germain OLEA <djuhngleen@gmail.com>
2021-04-23 12:47:56 +0200

    feat: update Teacher entity

--- server/src/main/java/entities/Teacher.java
+++ server/src/main/java/entities/Teacher.java
@@ -1,68 +1,29 @@
 package entities;
 
-import jakarta.persistence.Basic;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
 @Entity
+@Data
 public class Teacher extends User{
-    private String NUMEN;
+    @Id @Column(name = "ID", nullable = false)
     private int id;
+
+    @Basic @Column(name = "NUMEN", nullable = false, length = 26)
     private String numen;
 
-    public Teacher(String nom, String prenom, String telephone, String email, String adresse1, String adresse2, String ville, String cp, String NUMEN) {
+    @OneToMany(mappedBy = "teacher")
+    private List<Activity> activities;
+
+    public Teacher(String nom, String prenom, String telephone, String email, String adresse1, String adresse2, String ville, String cp, String numen) {
         super(nom, prenom, telephone, email, adresse1, adresse2, ville, cp);
-        this.NUMEN = NUMEN;
-    }
-
-    public String getNUMEN() {
-        return NUMEN;
-    }
-
-    public void setNUMEN(String NUMEN) {
-        this.NUMEN = NUMEN;
-    }
-
-    @Id
-    @Column(name = "ID", nullable = false)
-    public int getId() {
-        return id;
-    }
-
-    public _Dummy_ setId(int id) {
-        this.id = id;
-        return this;
-    }
-
-    @Basic
-    @Column(name = "NUMEN", nullable = false, length = 26)
-    public String getNumen() {
-        return numen;
-    }
-
-    public _Dummy_ setNumen(String numen) {
         this.numen = numen;
-        return this;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Teacher teacher = (Teacher) o;
-
-        if (id != teacher.id) return false;
-        if (numen != null ? !numen.equals(teacher.numen) : teacher.numen != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (numen != null ? numen.hashCode() : 0);
-        return result;
+    public Teacher() {
     }
 }
