e35051c90890da89f570f0229c0e712413011125
soveaux sebastien <sebastien.soveaux@etudiant.univ-reims.fr>
2021-04-23 12:16:27 +0200

    échanges entre serveur et client.

--- client/src/main/java/courses/client/app/ClientExchanges.java
+++ client/src/main/java/courses/client/app/ClientExchanges.java
@@ -1,66 +1,62 @@
 package courses.client.app;
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ClientExchanges {
-    final static String serverHost = "localhost";
-    public static void main(String args[]){
-        testToJsonClass test= new testToJsonClass();
-        Object[] array ={test};
-        String[] res = sendSocket(array);
-    }
 
-    public static String[] sendSocket(Object[] ObjectsToSend){
+    public static void main(String[] args) {
+
+        // Server Host
+        final String serverHost = "localhost";
+
         Socket socketOfClient = null;
         BufferedWriter os = null;
         BufferedReader is = null;
-        List<String> dataReceived = new ArrayList<String>();
 
         try {
             // Send a request to connect to the server is listening
-            // on machine 'localhost' port 7777.
-            socketOfClient = new Socket(serverHost, 7777);
+            // on machine 'localhost' port 9999.
+            socketOfClient = new Socket(serverHost, 1024);
 
             // Create output stream at the client (to send data to the server)
             os = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
+
 
             // Input stream at Client (Receive data from the server).
             is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
 
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + serverHost);
+            return;
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to " + serverHost);
+            return;
         }
 
         try {
-            String stringValue;
-            for( Object value : ObjectsToSend ) {
 
-                stringValue = testJackson.objectToJson(value);
-                // Write data to the output stream of the Client Socket.
-                os.write(stringValue);
+            // Write data to the output stream of the Client Socket.
+            os.write("HELO");
 
-                // End of line
-                os.newLine();
+            // End of line
+            os.newLine();
 
-                // Flush data.
-                os.flush();
-            }
-            os.write("end");
+            // Flush data.
+            os.flush();
+            os.write("I am Tom Cat");
+            os.newLine();
+            os.flush();
+            os.write("QUIT");
             os.newLine();
             os.flush();
 
+
+
             // Read data sent from the server.
             // By reading the input stream of the Client Socket.
-
             String responseLine;
             while ((responseLine = is.readLine()) != null) {
                 System.out.println("Server: " + responseLine);
-                dataReceived.add(responseLine);
-                if (responseLine.indexOf("end") != -1) {
+                if (responseLine.indexOf("OK") != -1) {
                     break;
                 }
             }
@@ -68,12 +64,10 @@ public class ClientExchanges {
             os.close();
             is.close();
             socketOfClient.close();
-            System.out.println("socket closed");
         } catch (UnknownHostException e) {
             System.err.println("Trying to connect to unknown host: " + e);
         } catch (IOException e) {
             System.err.println("IOException:  " + e);
         }
-        return dataReceived.toArray(new String[dataReceived.size()]);
     }
 }
