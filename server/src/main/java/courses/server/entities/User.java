5c51c0a48052fb2f453bf555631e3a73c5d9dde5
Germain OLEA <djuhngleen@gmail.com>
2021-04-26 16:00:29 +0200

    feat: refactor packages, add security

--- server/src/main/java/courses/server/entities/User.java
+++ server/src/main/java/courses/server/entities/User.java
@@ -4,52 +4,46 @@ import courses.server.security.RolesEnum;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import javax.persistence.Entity;
+
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
-@DiscriminatorColumn(name="TYPE", discriminatorType=DiscriminatorType.STRING, length=20)
+@DiscriminatorColumn(name="TYPE", discriminatorType=DiscriminatorType.STRING,length=20)
 @DiscriminatorValue("user")
 @Data
-public class User {
-    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+public class User extends Aggregator{
+    @Id @GeneratedValue
     @Column(name = "ID", nullable = false)
     private Integer id;
 
     @Basic @Column(name = "EMAIL", nullable = false, length = 50)
     private String email;
 
-    @Basic @Column(name = "PASSWORD_HASH", nullable = false)
     private String passwordHash;
 
-    @Column(name = "ROLE", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private RolesEnum role = RolesEnum.USER;
+    @Basic @Column(name = "ROLES", nullable = false)
+    private RolesEnum[] roles;
 
-    @Basic @Column(name = "SALT", nullable = false)
-    private String salt;
+    @Basic @Column(name = "TELEPHONE", nullable = false, length = 25)
+    private String telephone;
 
-    @Basic @Column(name = "VALIDATED", nullable = false)
-    private boolean validated = false;
+    @Basic @Column(name = "ADRESSE1", nullable = false, length = 25)
+    private String adresse1;
 
-    @Basic @Column(name = "TELEPHONE", length = 25)
-    private String telephone = null;
+    @Basic @Column(name = "ADRESSE2", nullable = true, length = 25)
+    private String adresse2;
 
-    @Basic @Column(name = "ADRESSE1", length = 25)
-    private String adresse1 = null;
+    @Basic @Column(name = "VILLE", nullable = false, length = 25)
+    private String ville;
 
-    @Basic @Column(name = "ADRESSE2", length = 25)
-    private String adresse2 = null;
-
-    @Basic @Column(name = "VILLE", length = 25)
-    private String ville = null;
-
-    @Basic @Column(name = "CP", length = 25)
-    private String cp = null;
+    @Basic @Column(name = "CP", nullable = false, length = 25)
+    private String cp;
 
     @Basic @Column(name = "NOM", nullable = false, length = 25)
-    private String nom = null;
+    private String nom;
 
     @Basic @Column(name = "PRENOM", nullable = false, length = 25)
-    private String prenom = null;
+    private String prenom;
 
     public User() {
     }
