0f6ccf45538e04985ea4c3fa9033fe6ad06fe0df
Germain OLEA <djuhngleen@gmail.com>
2021-04-28 23:19:31 +0200

    feat: update sql add validating account

--- server/src/main/java/courses/server/entities/User.java
+++ server/src/main/java/courses/server/entities/User.java
@@ -14,40 +14,44 @@ import javax.persistence.Entity;
 public class User extends Aggregator{
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "ID", nullable = false)
-    private Integer id = 0;
+    private Integer id;
 
     @Basic @Column(name = "EMAIL", nullable = false, length = 50)
-    private String email = "";
+    private String email;
 
-    private String passwordHash = "";
+    @Basic @Column(name = "PASSWORD_HASH", nullable = false)
+    private String passwordHash;
 
     @Column(name = "ROLE", nullable = false)
     @Enumerated(EnumType.STRING)
     private RolesEnum role = RolesEnum.USER;
 
     @Basic @Column(name = "SALT", nullable = false)
-    private String salt = "";
+    private String salt;
+
+    @Basic @Column(name = "VALIDATED", nullable = false)
+    private boolean validated = false;
 
     @Basic @Column(name = "TELEPHONE", length = 25)
-    private String telephone = "";
+    private String telephone = null;
 
     @Basic @Column(name = "ADRESSE1", length = 25)
-    private String adresse1 = "";
+    private String adresse1 = null;
 
     @Basic @Column(name = "ADRESSE2", length = 25)
-    private String adresse2 = "";
+    private String adresse2 = null;
 
     @Basic @Column(name = "VILLE", length = 25)
-    private String ville = "";
+    private String ville = null;
 
     @Basic @Column(name = "CP", length = 25)
-    private String cp = "";
+    private String cp = null;
 
     @Basic @Column(name = "NOM", nullable = false, length = 25)
-    private String nom = "";
+    private String nom = null;
 
     @Basic @Column(name = "PRENOM", nullable = false, length = 25)
-    private String prenom = "";
+    private String prenom = null;
 
     public User() {
     }
